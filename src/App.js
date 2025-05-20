c29d82b7748d4f5d0cf2748998804889e22c7ba3
hidalgo117 <sergox377@gmail.com>
2025-05-20 17:39:27 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Iniciar sesión</h2>
+        <form action="/login" method="post">
+        <label for="username">Usuario:</label><br></br>
+        <input type="text" id="username" name="username"></input><br></br>
+        <label for="password">Contraseña:</label><br></br>
+        <input type="password" id="password" name="password"></input><br></br>
+        <button type="submit">Iniciar sesión</button><br></br>
+  </form>
       </header>
     </div>
   );
 }
-
 export default App;
